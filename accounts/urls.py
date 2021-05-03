e3a19fc99ffa0ccdd43b313409512d9720ef4b41
christian acevedo <christian.acevedo14@upr.edu>
2021-05-02 22:52:57 -0400

    login and logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("registerLawyer", views.registerLawyer,name='registerLawyer'),
-    path("registerClient",views.registerClient,name='registerClient')
+    path("registerClient",views.registerClient,name='registerClient'),
+    path("login",views.login,name='login'),
+    path("logout",views.logout,name='logout')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from .models import User,Lawyer,Client
+from django.contrib.auth.models import auth
 # Create your views here.
 
 
@@ -37,3 +38,23 @@ def registerClient(request):
         return redirect('/')
     else:
         return render(request, 'ClientRegister.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username,password=password)
+
+        if user is not None:
+            auth.login(request,user)
+            return redirect("/")
+        else:
+            return redirect('login')
+    else:
+        return render(request,"login.html")
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
